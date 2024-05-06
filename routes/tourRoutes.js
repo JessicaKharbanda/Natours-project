396f78d8b2dec30095579e2b27dadfbf6c2a5979
Jessica Kharbanda <jessicakharbanda@Jessicas-MacBook-Pro.local>
2024-05-06 10:42:36 +0530

    final api

---
+++ routes/tourRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
+
+const Router = express.Router();
+
+// Router.param('id', tourController.checkId);
+
+Router.route('/top-5-cheap').get(
+  tourController.aliasTopTours,
+  tourController.getAllTours,
+);
+
+Router.route('/tour-stats').get(tourController.getTourStats);
+
+Router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+Router.route('/')
+  .get(authController.protect, tourController.getAllTours)
+  .post(tourController.createTour);
+
+Router.route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour,
+  );
+
+module.exports = Router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const Router = express.Router();
+
+Router.post('/signup', authController.signup);
+Router.post('/login', authController.login);
+
+Router.post('/forgotPassword', authController.forgotPassword);
+Router.patch('/resetPassword/:token', authController.resetPassword);
+
+Router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword,
+);
+
+Router.patch('/updateMe', authController.protect, userController.updateMe);
+Router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+Router.route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+Router.route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = Router;
